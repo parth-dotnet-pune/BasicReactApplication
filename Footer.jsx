7c5da5a531b547fb14fd799258b2e621d6cb1492
parth-dotnet-pune <parthkulkarni6442@gmail.com>
2025-11-18 14:25:28 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={styles.footer}>
+      <p>© 2025 React Assignment Footer</p>
+    </footer>
+  );
+}
+
+const styles = {
+  footer: {
+  width: "100%",
+  backgroundColor: "#222",
+  color: "#fff",
+  padding: "10px",
+  textAlign: "center",
+  marginTop: "30px",
+},
+
+};
+
+export default Footer;
